70b943b0c2807742dae5b5038eaa8856036a76fc
yangjitang77 <yangjitang77@gmail.com>
2017-09-21 14:32:43 +0800

    implement group#destroy

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -29,6 +29,13 @@ class GroupsController < ApplicationController
 
   end
 
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+
+    redirect_to groups_path ,alert: "Group delete"
+  end
+
   private
 
   def group_params
